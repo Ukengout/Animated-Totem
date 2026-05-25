1664113b3cfce5223941c120e2cd9a60a4fe5db3
Ukengout <kkot90484@gmail.com>
2026-05-25 09:33:19 +0300

    Delete js directory

--- js/layout-boot.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var m = sessionStorage.getItem("totemLayout");
-  var mob = m === "mobile" || (m !== "desktop" && window.innerWidth <= 768);
-  document.documentElement.classList.toggle("layout-mobile-active", mob);
-  document.documentElement.classList.toggle("layout-desktop-active", !mob);
-})();
